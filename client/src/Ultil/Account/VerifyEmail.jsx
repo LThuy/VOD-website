a8c087680169eb902a8cbdc38ec05e7bd83db8a9
thinhisme1123 <kieplulanh93@gmail.com>
2025-02-07 23:30:23 +0700

    Update VerifyEmail.jsx

--- client/src/Ultil/Account/VerifyEmail.jsx
+++ client/src/Ultil/Account/VerifyEmail.jsx
@@ -12,7 +12,7 @@ const VerifyEmail = () => {
 
         const verifyToken = async () => {
             try {
-                const response = await axios.get(`http://localhost:5000/verify-email?token=${token}`);
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_BASE_URL}/verify-email?token=${token}`);
                 const message = response.data.message;
                 navigate('/successnotice', { state: { message } });
             } catch (error) {
